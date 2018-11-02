a4cbc62bad31c0561454d5cbcd619b1fb1f828a2
Moritz Staffel <m.staffel@deersoft.de>
2018-11-02 10:42:37 +0100

    Add osx impl

--- src/Prefix/StdAfx.h
+++ src/Prefix/StdAfx.h
@@ -35,8 +35,8 @@ using namespace VectorworksMVR;
 #define ASSERTN(x,y)  if(bool(y) == false) { __PrintDebugValue__(x, y); }
 #define DSTOP(params)                { __PrintDebugValue2__(params); }
 #define VERIFYN
-#define VWFC_ASSERT(x,y)  if(bool(y) == false) { __PrintDebugValue__(x, y); }
-#define THROW_VWFC_EXCEPTION
+#define VWFC_ASSERT(x)  if(bool(x) == false) { __PrintDebugValue2__(x); }
+#define THROW_VWFC_EXCEPTION(x,y,z ) { __PrintDebugValue__(x, z); }
 
 #define kEveryone 1
 #define kMCCoordTypes "2"
