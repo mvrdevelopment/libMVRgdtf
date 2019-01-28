cae4ce03905cd6d52e154f29ed2b8e00179d2460
Moritz Staffel <m.staffel@deersoft.de>
2019-01-28 12:08:13 +0100

    Created Target for vs2015 (release & debug) (#70)
    Add VS2015 Target

--- runMSBuild.bat
+++ runMSBuild.bat
@@ -1,3 +1,3 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat"
-MSBuild.exe libVectorworksMvrGdtf.sln /t:Build /p:Configuration=Release
+MSBuild.exe libVectorworksMvrGdtf.sln /t:Build /p:Configuration=Release /p:Platform=x64
 x64\Release\libVectorworksMvrGdtfTestApp.exe
