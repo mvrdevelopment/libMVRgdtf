d68afd9cdc0e940095dffa67e9b18ef0066363e9
Patrick Winkler <p.winkler@deersoft.de>
2019-02-20 17:12:49 +0100

    Update CGdtfDMXPersonality.h

--- src/CGdtfDMXPersonality.h
+++ src/CGdtfDMXPersonality.h
@@ -5,7 +5,31 @@
 
 #include "GDTFManager.h"
 
+
 namespace VectorworksMVR
 {
+    class CGdtfDMXPersonalityImpl : public VectorworksMVR::VCOMImpl<IGdtfDMXPersonality>
+    {
+    public:
+        CGdtfDMXPersonalityImpl();
+        virtual ~CGdtfDMXPersonalityImpl();
 
+        // DMXPersonality_Optional virtual VCOMError VCOM_CALLTYPE     GetDMXPersonalityCount(size_t& outCount);
+        // DMXPersonality_Optional virtual VCOMError VCOM_CALLTYPE     GetDMXPersonalityAt(size_t at, IGdtfDMXPersonality**  outDMXPersonality);
+        // DMXPersonality_Optional virtual VCOMError VCOM_CALLTYPE     CreateDMXPersonality (IGdtfDMXPersonality** outDMXPersonality);		
+
+        virtual VCOMError VCOM_CALLTYPE     BindToObject(void* objAddr);
+        virtual void*	  VCOM_CALLTYPE     GetBoundObject();
+
+    private:
+        SceneData::GdtfDMXPersonality* fDMXPersonality;
+
+    public:
+        void					        SetPointer(SceneData::GdtfDMXPersonality* DMXPersonality);
+        SceneData::GdtfDMXPersonality*	GetPointer();
+
+    };
+    
+    const VWIID IID_GdtfDMXPersonality = { 0xe24395d7, 0xfda9, 0x4fae, {0xb8, 0x3b, 0xac, 0xe8, 0x38, 0x24, 0x25, 0x83}};
 }
+
