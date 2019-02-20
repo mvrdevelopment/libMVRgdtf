45a06a7291f532ce9346f43a249c7ffd61ec5bd6
Patrick Winkler <p.winkler@deersoft.de>
2019-02-20 18:46:49 +0100

    Add Files CGdtfSoftwareVersionID.h / .cpp

---
+++ src/Implementation/CGdtfSoftwareVersionID.h
@@ -0,0 +1,8 @@
+//-----------------------------------------------------------------------------
+//----- Copyright deersoft 2015 - 2018 www.deersoft.de
+//-----------------------------------------------------------------------------
+#pragma once
+
+#include "Include/IMediaRessourceVectorInterface.h"
+#include "GDTFManager.h"
+
