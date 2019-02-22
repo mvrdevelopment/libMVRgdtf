e0d2277c5acffac135bf18207303ccc38645e829
Patrick Winkler <p.winkler@deersoft.de>
2019-02-22 22:11:30 +0100

    Update main.cpp

--- unittest/main.cpp
+++ unittest/main.cpp
@@ -11,25 +11,13 @@
 #include "GdtfFunctionModeMaster.h"
 #include "Utility.h"
 
-#if defined(_WINDOWS)
-# include <Shlobj.h>
-# include <Shlwapi.h>
-#elif GS_LIN
-#include <unistd.h>
-#include <sys/types.h>
-#include <pwd.h>
-#elif GS_MAC
-#include <stdio.h> 
-#endif
 
 using namespace VectorworksMVR;					
-
-bool GetFolderAppDataPath(std::string& outPath);
 
 int main(int argc, char* argv[])
 {
 	// Get Current dir    
-	std::string base; GetFolderAppDataPath(base);    
+	std::string base; UnitTestUtil::GetFolderAppDataPath(base);
 
 	GdtfUnittest gdtfTest(base);
 	bool gdtfFailed = gdtfTest.RunTest();
@@ -60,34 +48,4 @@ int main(int argc, char* argv[])
     return errorOcurred;
 }
 
-bool GetFolderAppDataPath(std::string& outPath)
-{
-	//--------------------------------------------------------
-	// Implementation for Windows
-#ifdef _WINDOWS
-    
-    // Leave this empty for now
-	// Beware, brain-compiled code ahead!
-	wchar_t buffer[MAX_PATH];
-	HWND hWnd = NULL;
-	
-	BOOL result = SHGetSpecialFolderPath(hWnd, buffer, CSIDL_LOCAL_APPDATA, false );
-	
-	if(!result) return false;
-    std::wstring ws(buffer);
-	outPath = std::string(ws.begin(), ws.end());
-    
-#elif _LINUX
-	// LINUX_IMPLEMENTATION - done
-	struct passwd *pw = getpwuid(getuid());
 
-	const char *homedir = pw->pw_dir;
-	outPath = std::string(homedir);
-#elif GS_MAC
-	//Implementation for OSX
-	const char *homeDir = getenv("HOME");
-	outPath = std::string(homeDir);
-#endif
-	
-	return true;
-}
