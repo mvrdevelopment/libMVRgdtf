15d3261cf174c515dbbe23da36c8d6bf3ceb2d05
Patrick Winkler <p.winkler@deersoft.de>
2019-02-27 21:20:51 +0100

    CGdtfFilterImpl

--- src/Implementation/CGdtfFilter.cpp
+++ src/Implementation/CGdtfFilter.cpp
@@ -2,7 +2,46 @@
 //----- Copyright deersoft 2015 - 2017 www.deersoft.de
 //-----------------------------------------------------------------------------
 #include "Prefix/StdAfx.h"
+#include "CGdtfFilter.h"
+
+using namespace VectorworksMVR;
 
 
-using namespace VectorWorks::Filing;
+CGdtfFilterImpl::CGdtfFilterImpl()
+{
+    fFilter = nullptr;
+}
+
+VectorworksMVR::CGdtfFilterImpl::~CGdtfFilterImpl()
+{
+}
+
+VCOMError VCOM_CALLTYPE VectorworksMVR::CGdtfFilterImpl::BindToObject(void * objAddr)
+{
+    // Check if valid
+    if (!fFilter) return kVCOMError_NotInitialized;
+
+    fFilter->SetBind(objAddr);
+
+    return kVCOMError_NoError;
+}
+
+void *VCOM_CALLTYPE VectorworksMVR::CGdtfFilterImpl::GetBoundObject()
+{
+    // Check if valid
+    if (!fFilter) return nullptr;
+
+    return fFilter->GetBind();
+}
+
+void VectorworksMVR::CGdtfFilterImpl::SetPointer(SceneData::GdtfFilter * val)
+{
+    fFilter = val;
+}
+
+SceneData::GdtfFilter * VectorworksMVR::CGdtfFilterImpl::GetPointer()
+{
+    return fFilter;
+}
+
 
--- src/Implementation/CGdtfFilter.h
+++ src/Implementation/CGdtfFilter.h
@@ -2,7 +2,29 @@
 //----- Copyright deersoft 2015 - 2018 www.deersoft.de
 //-----------------------------------------------------------------------------
 #pragma once
-
 #include "Include/IMediaRessourceVectorInterface.h"
 #include "GDTFManager.h"
 
+namespace VectorworksMVR
+{
+    class CGdtfFilterImpl : public VCOMImpl<IGdtfFilter>
+    {
+    public:
+        CGdtfFilterImpl();
+        virtual ~CGdtfFilterImpl();
+
+
+        virtual VCOMError VCOM_CALLTYPE     BindToObject(void* objAddr);
+        virtual void*	  VCOM_CALLTYPE     GetBoundObject();
+
+    private:
+        SceneData::GdtfFilter*  fFilter;
+
+    public:
+        void					SetPointer(SceneData::GdtfFilter* val);
+        SceneData::GdtfFilter*	GetPointer();
+
+    };
+
+    const VWIID IID_GdtfFilter  = { 0x66f1d4b2, 0xf040, 0x4d4e, {0xb1, 0x54, 0x4c, 0xcb, 0x2b, 0x68, 0xc2, 0xc0}};
+}
