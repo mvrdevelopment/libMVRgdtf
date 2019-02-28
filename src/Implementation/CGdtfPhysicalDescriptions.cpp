4133c557c2f38660633dc3433a2957e4fac9e2d0
Patrick Winkler <p.winkler@deersoft.de>
2019-02-28 14:35:54 +0100

    CGdtfPhysicalDescriptionsImpl

--- src/Implementation/CGdtfPhysicalDescriptions.cpp
+++ src/Implementation/CGdtfPhysicalDescriptions.cpp
@@ -2,3 +2,43 @@
 //----- Copyright deersoft 2015 - 2017 www.deersoft.de
 //-----------------------------------------------------------------------------
 #include "Prefix/StdAfx.h"
+#include "CGdtfPhysicalDescriptions.h"
+
+
+CGdtfPhysicalDescriptionsImpl::CGdtfPhysicalDescriptionsImpl()
+{
+    fPhysicalDescriptions = nullptr;
+}
+
+CGdtfPhysicalDescriptionsImpl::~CGdtfPhysicalDescriptionsImpl()
+{
+}
+
+VCOMError VCOM_CALLTYPE VectorworksMVR::CGdtfPhysicalDescriptionsImpl::BindToObject(void * objAddr)
+{
+    // Check if valid
+    if (!fPhysicalDescriptions) return kVCOMError_NotInitialized;
+
+    fPhysicalDescriptions->SetBind(objAddr);
+
+    return kVCOMError_NoError;
+}
+
+void *VCOM_CALLTYPE VectorworksMVR::CGdtfPhysicalDescriptionsImpl::GetBoundObject()
+{
+    // Check if valid
+    if (!fPhysicalDescriptions) return nullptr;
+
+    return fPhysicalDescriptions->GetBind();
+}
+
+void VectorworksMVR::CGdtfPhysicalDescriptionsImpl::SetPointer(SceneData::GdtfPhysicalDescriptions* val)
+{
+    fPhysicalDescriptions = val;
+}
+
+SceneData::GdtfPhysicalDescriptions * VectorworksMVR::CGdtfPhysicalDescriptionsImpl::GetPointer()
+{
+    return fPhysicalDescriptions;
+}
+
--- src/Implementation/CGdtfPhysicalDescriptions.h
+++ src/Implementation/CGdtfPhysicalDescriptions.h
@@ -6,3 +6,24 @@
 #include "Include/IMediaRessourceVectorInterface.h"
 #include "GDTFManager.h"
 
+namespace VectorworksMVR
+{
+    class CGdtfPhysicalDescriptionsImpl : public VCOMImpl<IGdtfPhysicalDescriptions>
+    {
+    public:
+        CGdtfPhysicalDescriptionsImpl();
+        virtual ~CGdtfPhysicalDescriptionsImpl();
+
+        virtual VCOMError VCOM_CALLTYPE     BindToObject(void* objAddr);
+        virtual void*	  VCOM_CALLTYPE     GetBoundObject();
+
+    private:
+        SceneData::GdtfPhysicalDescriptions*   fPhysicalDescriptions;
+
+    public:
+        void					                SetPointer(SceneData::GdtfPhysicalDescriptions* val);
+        SceneData::GdtfPhysicalDescriptions*	GetPointer();
+    };
+
+    const VWIID IID_GdtfPhysicalDescriptions = { 0x80683668, 0x1f68, 0x4a91, {0xb8, 0x9e, 0xb9, 0x8d, 0x36, 0x74, 0x08, 0x96}};
+}
