947b882af72c3dc6481bce16c1436c238d922e1d
Moritz Staffel <m.staffel@deersoft.de>
2019-03-21 19:53:15 +0100

    Add function

--- unittest/XmlConversionTest.cpp
+++ unittest/XmlConversionTest.cpp
@@ -27,7 +27,14 @@ bool XmlConversionTest::ExecuteTest()
     return true;
 }
 
+void CheckDmxValue(const char* string, DmxValue result, EGdtfChannelBitResolution targetResolution, XmlConversionTest* ref)
+{
+	DmxValue value = 0;
+	ref->checkifTrue("DMX Conversion faild" ,GdtfConverter::ConvertDMXValue(string, nullptr, targetResolution, value) );
+	ref->checkifEqual("DmxValue", value, result);
+};
+
 void XmlConversionTest::RunTests()
 {
-	GdtfConverter::ConvertDMXValue(15, EGdtfChannelBitResolution::eGdtfChannelBitResolution_8);
+	CheckDmxValue("255/1", 255, EGdtfChannelBitResolution::eGdtfChannelBitResolution_8, this);
 }
