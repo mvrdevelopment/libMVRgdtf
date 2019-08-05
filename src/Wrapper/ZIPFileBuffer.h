997734f43ed1ae4b8d41eb367f9f82fe2cf77484
Deniz Sharideh <deniz.sharideh@gmail.com>
2019-08-05 11:20:28 +0200

    Remove this

--- src/Wrapper/ZIPFileBuffer.h
+++ src/Wrapper/ZIPFileBuffer.h
@@ -11,14 +11,14 @@ namespace VectorworksMVR
 		{
 		public:
 			ZIPFileBuffer();
-			virtual	~ZIPFileBuffer();
+			~ZIPFileBuffer();
 
 		public:
-			virtual VCOMError VCOM_CALLTYPE	Open(VectorworksMVR::Filing::IFileIdentifier* pFileID,  bool openForRead);
-			virtual VCOMError VCOM_CALLTYPE	Close();
-			virtual VCOMError VCOM_CALLTYPE	GetFileSize(Uint64& outValue);
-			virtual VCOMError VCOM_CALLTYPE	Read(Uint64 position, Uint64& inoutSize, void* pOutBuffer);
-			virtual VCOMError VCOM_CALLTYPE	Write(Uint64 position, Uint64 size, const void* pBuffer);
+			VCOMError	Open(VectorworksMVR::Filing::IFileIdentifier* pFileID,  bool openForRead);
+			VCOMError	Close();
+			VCOMError	GetFileSize(Uint64& outValue);
+			VCOMError 	Read(Uint64 position, Uint64& inoutSize, void* pOutBuffer);
+			VCOMError 	Write(Uint64 position, Uint64 size, const void* pBuffer);
 
 		private:
 			Uint8*						fpZIPFileBuffer;
