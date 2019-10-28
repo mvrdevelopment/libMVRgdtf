3d63349efd2c27370e00f84fcbafe216c82ddecf
Moritz Staffel <m.staffel@deersoft.de>
2019-10-28 15:25:36 +0100

    Update GDTFManager.cpp

--- src/GDTFManager.cpp
+++ src/GDTFManager.cpp
@@ -4665,8 +4665,9 @@ GdtfFixture::GdtfFixture(IFileIdentifierPtr inZipFile, TXString folderName)
             
             // Seperate filename und folder
             TXString fileNameWithoutFolder = TXString(fileName).Replace("/", TXString(kSeperator) );	
+#ifndef _WINDOWS
 			fileNameWithoutFolder          = TXString(fileName).Replace("\\", TXString(kSeperator) ); // It seems some zips come also with this.
-
+#endif
             TXString subFolder 			   = SystemUtil::ExtractFolderFromPath(fileNameWithoutFolder);        
             
 			// flatten the folder structure
