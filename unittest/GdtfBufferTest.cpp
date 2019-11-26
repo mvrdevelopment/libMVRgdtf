a0fbb0a7fc0b95d780a35ac2a069636c1744a33c
Moritz Staffel <m.staffel@deersoft.de>
2019-11-26 11:57:04 +0100

    Add Buffer Unit Test

--- unittest/GdtfBufferTest.cpp
+++ unittest/GdtfBufferTest.cpp
@@ -9,6 +9,13 @@
 using namespace VectorworksMVR;
 using namespace VectorworksMVR::GdtfDefines;
 
+#include <cstdio>
+
+#ifdef GS_LIN
+#include <unistd.h>
+#include <stdio.h>
+#endif
+
 
 #define __checkVCOM(x) this->checkVCOM(x, #x)
 #define __checkVCOM_NotSet(x) this->checkVCOM_NotSet(x, #x)
@@ -16,6 +23,7 @@ using namespace VectorworksMVR::GdtfDefines;
 
 GdtfBufferTest::GdtfBufferTest(const std::string& currentDir)
 {
+	
 }
 
 GdtfBufferTest::~GdtfBufferTest()
@@ -26,6 +34,30 @@ bool GdtfBufferTest::ExecuteTest()
 {
 	std::cout << "=                                        GdtfBufferTest                                      =" << std::endl;
 
+	std::string path 		= UnitTestUtil::GetTestResourceFolder() + kSeparator + "allWorking.gdtf";
+	std::FILE*	gdtfFile 	= std::fopen( path.c_str(), "rb" );
+	
+	// Get Buffer
+	size_t length = 0;
+	if ( std::fseek( gdtfFile, 0, SEEK_END ) == 0 )
+	{
+		length = std::ftell( gdtfFile );
+	}
+
+	// Get Buffer
+	char*  buffer  = new char[length + 1];
+	if ( std::fseek( gdtfFile, 0, SEEK_SET ) == 0 )
+	{
+		std::fread( buffer, 1, length, gdtfFile );		
+	}
+
+
+	// Try to Read It
+	IGdtfFixturePtr gdtfFixture (IID_IGdtfFixture);
+	if(__checkVCOM(gdtfFixture->FromBuffer(buffer, length)))
+	{
+
+	}
 
     return true;
 }
