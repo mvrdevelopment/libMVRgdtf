f5a2199a323db6e17b34fb7c6667149dc91635b7
J4m3s00 <james.lol2000@gmail.com>
2020-07-06 17:44:00 +0200

    Little win test in python script

--- scripts/build_xerces.py
+++ scripts/build_xerces.py
@@ -1,3 +1,4 @@
+import sys
 import os.path
 import urllib
 import zipfile
@@ -28,5 +29,9 @@ if not os.path.isfile("shared/lib/libxerces-c.a"):
         os.mkdir("build")
     os.chdir("build")
 
-    os.system("cmake -DBUILD_SHARED_LIBS:BOOL=OFF -DCMAKE_INSTALL_PREFIX:PATH=libs -Dxmlch-type=wchar_t ..")
+    extraCmakeOptions = "";
+    if sys.platform == "win32":
+        extraCmakeOptions += "-Dxmlch-type=wchar_t"
+
+    os.system("cmake -DBUILD_SHARED_LIBS:BOOL=OFF -DCMAKE_INSTALL_PREFIX:PATH=libs "+ extraCmakeOptions +" ..")
     os.system("cmake --build . --target install")
