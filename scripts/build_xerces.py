208f279872c4aecbc21db5100955f5b1ea500243
J4m3s00 <james.lol2000@gmail.com>
2020-07-07 15:34:47 +0200

    Fix travis windows build

--- scripts/build_xerces.py
+++ scripts/build_xerces.py
@@ -9,9 +9,11 @@ XERCES_VERSION = "3.2.2"
 XERCES_ZIP_FILE = "shared/xerces-c-" + XERCES_VERSION + ".zip"
 XERCES_DIR_PATH = "shared/xerces-c-" + XERCES_VERSION
 
+if sys.platform == "win32":
+    pathToLibFile = ""
 
 # check for other system lib file (.lib [for windows])
-if not os.path.isfile("libs/libxerces-c.a"):
+if not os.path.isfile(pathToLibFile):
     if not os.path.isfile(XERCES_ZIP_FILE):
         print("Downloading xerces")
         urllib.urlretrieve("http://archive.apache.org/dist/xerces/c/3/sources/xerces-c-" + XERCES_VERSION + ".zip", XERCES_ZIP_FILE)
@@ -29,7 +31,7 @@ if not os.path.isfile("libs/libxerces-c.a"):
             os.mkdir("build")
         os.chdir("build")
 
-        extraCmakeOptions += "-DBUILD_SHARED_LIBS:BOOL=OFF -DCMAKE_INSTALL_PREFIX:PATH=libs -Dxmlch-type=wchar_t"
+        extraCmakeOptions += "-DBUILD_SHARED_LIBS:BOOL=OFF -DCMAKE_INSTALL_PREFIX:PATH=libs -Dxmlch-type=wchar_t -DCMAKE_CXX_FLAGS_RELEASE:STRING=Release"
         os.system("cmake "+ extraCmakeOptions +" ..")
         os.system("cmake --build . --target install")
     #we need some more cases for linux and should check all configure-options
