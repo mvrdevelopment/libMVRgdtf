1f30f6590abdb080888bb51fe17bc252b9045b47
J4m3s00 <james.lol2000@gmail.com>
2020-07-07 17:15:07 +0200

    Little ref

--- runMSBuild_lib.bat
+++ runMSBuild_lib.bat
@@ -1,3 +1,4 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat"
 REM build libVectorworksMvrGdtf
-MSBuild.exe build/MvrGdtf.sln /t:Build /p:Configuration=%1 /m
+cd build
+MSBuild.exe MvrGdtf.sln /t:Build /p:Configuration=%1 /m
