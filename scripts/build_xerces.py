6b07c318d78976bbb28d39f45fd53710d9f549dd
J4m3s00 <james.lol2000@gmail.com>
2020-07-07 13:08:09 +0200

    ref

--- scripts/build_xerces.py
+++ scripts/build_xerces.py
@@ -10,12 +10,8 @@ XERCES_ZIP_FILE = "shared/xerces-c-" + XERCES_VERSION + ".zip"
 XERCES_DIR_PATH = "shared/xerces-c-" + XERCES_VERSION
 
 
-
 # check for other system lib file (.lib [for windows])
 if not os.path.isfile("shared/lib/libxerces-c.a"):
-    print ("===============================================================")
-    print ("= M A K E   X e r c e s - C                                   =")
-    print ("===============================================================")
     if not os.path.isfile(XERCES_ZIP_FILE):
         print("Downloading xerces")
         urllib.urlretrieve("http://archive.apache.org/dist/xerces/c/3/sources/xerces-c-" + XERCES_VERSION + ".zip", XERCES_ZIP_FILE)
@@ -33,8 +29,8 @@ if not os.path.isfile("shared/lib/libxerces-c.a"):
             os.mkdir("build")
         os.chdir("build")
 
-        extraCmakeOptions += "-Dxmlch-type=wchar_t"
-        os.system("cmake -DBUILD_SHARED_LIBS:BOOL=OFF -DCMAKE_INSTALL_PREFIX:PATH=libs "+ extraCmakeOptions +" ..")
+        extraCmakeOptions += "-DBUILD_SHARED_LIBS:BOOL=OFF -DCMAKE_INSTALL_PREFIX:PATH=libs -Dxmlch-type=wchar_t"
+        os.system("cmake "+ extraCmakeOptions +" ..")
         os.system("cmake --build . --target install")
     #we need some more cases for linux and should check all configure-options
     if sys.platform == "darwin" or sys.platform.startswith("linux"):
