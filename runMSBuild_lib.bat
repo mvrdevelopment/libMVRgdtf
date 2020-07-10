b7fe6cb1858cc0037008a46f721b0180ee8cd0fc
J4m3s00 <james.lol2000@gmail.com>
2020-07-10 13:14:20 +0200

    Fixing windows... again

--- runMSBuild_lib.bat
+++ runMSBuild_lib.bat
@@ -2,3 +2,4 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Too
 REM build libVectorworksMvrGdtf
 cd build
 MSBuild.exe MvrGdtf.sln /t:Build /p:Configuration=%1 /m
+Release/MvrGdtfUnitTest.exe
