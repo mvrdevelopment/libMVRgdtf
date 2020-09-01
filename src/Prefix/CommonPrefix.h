8ad87bd51070fe91272e716cbc845af651795c1e
fbeasse <felix.beasse@telecom-paristech.fr>
2020-09-01 11:39:34 +0200

    Update CommonPrefix.h

--- src/Prefix/CommonPrefix.h
+++ src/Prefix/CommonPrefix.h
@@ -528,7 +528,7 @@ const Sint32 kMVR_MinorVersion = 4;
 #define XML_GDTF_ConnectorNodeName                      "Connector"
 #define XML_GDTF_ConnectorName                          "Name"
 #define XML_GDTF_ConnectorType                          "Type"
-#define XML_GDTF_ConnectorDmxBreak                      "DmxBreak"
+#define XML_GDTF_ConnectorDmxBreak                      "DMXBreak"
 #define XML_GDTF_ConnectorGender                        "Gender"
 #define XML_GDTF_ConnectorLength                        "Length"
 
