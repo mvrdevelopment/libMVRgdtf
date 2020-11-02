8cea533db64f21f39a973f0bebfdeb0fedc89f50
fbeasse <felix.beasse@telecom-paristech.fr>
2020-11-02 12:00:36 +0100

    Housekeeping in the TaggingDOMParser

--- src/XMLLib/TaggingDOMParser.cpp
+++ src/XMLLib/TaggingDOMParser.cpp
@@ -55,6 +55,7 @@ TaggingDOMParser::TaggingDOMParser() : dataHandler(new TagDataHandler())
 
 TaggingDOMParser::~TaggingDOMParser()
 {
+    delete dataHandler;
 }
 
 Tag* TaggingDOMParser::createTag()
