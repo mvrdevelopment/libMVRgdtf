0e9b8a97ffcb30e784fb6bf6e759e382524b6426
Moritz Staffel <m.staffel@deersoft.de>
2022-06-07 16:14:23 +0200

    Update GDTF Data version

--- src/Prefix/CommonPrefix.h
+++ src/Prefix/CommonPrefix.h
@@ -216,7 +216,7 @@ const Sint32 kMVR_MinorVersion = 4;
 
 #define XML_GDTF_ROOTNODENAME			    "GDTF"
 #define XML_GDTF_ROOTNODEATTRVERSION	    "DataVersion"
-#define XML_GDTF_CurrentVersion			    "1.1"
+#define XML_GDTF_CurrentVersion			    "1.2"
 
 #define XML_GDTF_FixtureNodeName		    "FixtureType"
 #define XML_GDTF_FixtureName			    "Name"
