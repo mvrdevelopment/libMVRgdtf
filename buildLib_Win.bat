f79cf300aa8c5edd6361b52ac1cf7c02eba2beeb
pwinkler-ds <38907502+pwinkler-ds@users.noreply.github.com>
2022-08-26 19:04:22 +0200

    wrong param

--- buildLib_Win.bat
+++ buildLib_Win.bat
@@ -27,7 +27,7 @@ cd ..
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat"
 REM build libVectorworksMvrGdtf
 cd build
-MSBuild.exe MvrGdtf.sln /t:Build /p:Configuration=%2 /m
+MSBuild.exe MvrGdtf.sln /t:Build /p:Configuration=%1 /m
 
 goto END
 
