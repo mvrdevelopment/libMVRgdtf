0879a4ea076ca68844a683266b49c02d4480f50b
pwinkler-ds <38907502+pwinkler-ds@users.noreply.github.com>
2022-08-26 19:02:56 +0200

    format

--- buildAllConfigs_Win.bat
+++ buildAllConfigs_Win.bat
@@ -4,22 +4,22 @@ call buildLib_Win.bat Release MT MZ
 popd
 pushd . 
 
-call buildLib_Win.bat Release MT NoMZ
-popd
-pushd . 
-
-call buildLib_Win.bat Release MD
-popd
-pushd . 
-
-
-call buildLib_Win.bat Debug MD
-popd
-pushd . 
-
-call buildLib_Win.bat Debug MT
-popd
-pushd . 
+:: call buildLib_Win.bat Release MT NoMZ
+:: popd
+:: pushd . 
+:: 
+:: call buildLib_Win.bat Release MD
+:: popd
+:: pushd . 
+:: 
+:: 
+:: call buildLib_Win.bat Debug MD
+:: popd
+:: pushd . 
+:: 
+:: call buildLib_Win.bat Debug MT
+:: popd
+:: pushd . 
 
 echo FINISHED
 
