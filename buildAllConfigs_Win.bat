cfdef88521f49151c93c7d545d843fe0bbe945e9
pwinkler-ds <38907502+pwinkler-ds@users.noreply.github.com>
2022-08-26 19:12:00 +0200

    add configurations

--- buildAllConfigs_Win.bat
+++ buildAllConfigs_Win.bat
@@ -4,22 +4,36 @@ call buildLib_Win.bat Release MT MZ
 popd
 pushd . 
 
-:: call buildLib_Win.bat Release MT NoMZ
-:: popd
-:: pushd . 
-:: 
-:: call buildLib_Win.bat Release MD
-:: popd
-:: pushd . 
-:: 
-:: 
-:: call buildLib_Win.bat Debug MD
-:: popd
-:: pushd . 
-:: 
-:: call buildLib_Win.bat Debug MT
-:: popd
-:: pushd . 
+call buildLib_Win.bat Release MT NoMZ
+popd
+pushd . 
+
+call buildLib_Win.bat Release MD MZ
+popd
+pushd . 
+
+call buildLib_Win.bat Release MD NoMZ
+popd
+pushd .
+
+
+call buildLib_Win.bat Debug MD MZ
+popd
+pushd . 
+
+call buildLib_Win.bat Debug MD NoMZ
+popd
+pushd . 
+
+
+call buildLib_Win.bat Debug MT MZ
+popd
+pushd . 
+
+call buildLib_Win.bat Debug MT NoMZ
+popd
+pushd . 
+
 
 echo FINISHED
 
