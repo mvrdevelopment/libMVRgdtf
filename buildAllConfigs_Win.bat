51c813daf6087238c5dbd32d19f67b0f15ff09d9
pwinkler-ds <38907502+pwinkler-ds@users.noreply.github.com>
2022-08-26 16:24:10 +0200

    automate lib build

---
+++ buildAllConfigs_Win.bat
@@ -0,0 +1,8 @@
+buildLib_Win.bat Release MD
+buildLib_Win.bat Release MT
+buildLib_Win.bat Debug MD
+buildLib_Win.bat Debug MT
+
+echo FINISHED
+
+pause
---
+++ buildLib_Win.bat
@@ -0,0 +1,34 @@
+:: This script expects the following arguments: 
+:: %1 : Type "Release" or "Debug" 
+:: %2 : Runtime Lib: "MD" or "MT"
+
+@echo off
+echo Provided arguments: %1 %2
+
+if %1.==. goto ERROR
+if %2.==. goto ERROR
+
+@echo on
+
+if exist build ( rd /S /Q build)
+
+mkdir build && cd build
+
+cmake -G "Visual Studio 15 2017 Win64" -DBUILD_SHARED_LIBS:BOOL=OFF -DCMAKE_INSTALL_PREFIX:PATH=libs -DCMAKE_CONFIGURATION_TYPES=%2 -Dxmlch-type=wchar_t -DCMAKE_CXX_FLAGS_RELEASE:STRING="%1 -O2 -Ob2 -DNDEBUG" ..
+
+cd ..
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat"
+REM build libVectorworksMvrGdtf
+cd build
+MSBuild.exe MvrGdtf.sln /t:Build /p:Configuration=%2 /m
+
+goto END
+
+:ERROR
+@echo off
+Color 04
+echo Not all parameters are provided for this script!
+
+:END
+pause
