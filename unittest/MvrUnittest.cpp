fb5bffd214f3dbb21a095081a503c39608483760
pwinkler-ds <p.winkler@deersoft.de>
2023-06-15 17:29:06 +0200

    getFailedTestCount

--- unittest/MvrUnittest.cpp
+++ unittest/MvrUnittest.cpp
@@ -36,7 +36,9 @@ bool MvrUnittest::ExecuteTest()
 {
 	std::cout << "=                                        MvrUnittest                                       =" << std::endl;
     WriteFile();
-    ReadFile();
+    ReadFile();    
+    
+    std::cout << this->getFailedTestCount() << "Tests have failed."; 
 
     return true;
 }
