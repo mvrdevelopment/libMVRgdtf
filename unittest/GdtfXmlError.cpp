f21ca5dbd57a1f824d30cba4faf4d69104026db1
Moritz Staffel <m.staffel@deersoft.de>
2023-07-11 09:26:57 +0200

    Update GdtfXmlError.cpp

--- unittest/GdtfXmlError.cpp
+++ unittest/GdtfXmlError.cpp
@@ -46,7 +46,7 @@ void GdtfXmlErrorTest::ReadDamagedFile()
 #ifdef DONT_USE_XERCES_AS_XMLLIB
 	checkifEqual("Count Errors", countErrors, (size_t)2);
 #else
-	checkifEqual("Count Errors", countErrors, (size_t)1);
+	checkifEqual("Count Errors", countErrors, (size_t)2);
 #endif
 	for(size_t i = 0; i < countErrors; i++)
 	{
@@ -58,6 +58,7 @@ void GdtfXmlErrorTest::ReadDamagedFile()
 		if(i == 1) { ReadError(error, 4, 268, GdtfDefines::EGdtfParsingError::eNodeMissingMandatoryAttribute, "FixtureType", "My FixtureName"); }
 #else
 		if(i == 0) { ReadError(error, 27, 7, GdtfDefines::EGdtfParsingError::eXmlParsingError, "", ""); }
+		if(i == 1) { ReadError(error, 4, 268, GdtfDefines::EGdtfParsingError::eNodeMissingMandatoryAttribute, "FixtureType", "My FixtureName"); }
 		//tinyxml does not open the file, if an error occured
 #endif
 	}
