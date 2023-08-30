4656cde7001c2f124d7101996046fa8db13bad65
Mark-André Schadow <markandre@hotmail.de>
2023-08-30 13:48:52 +0200

    u

--- src/Include/IMediaRessourceVectorInterface.h
+++ src/Include/IMediaRessourceVectorInterface.h
@@ -1916,7 +1916,7 @@ class DYNAMIC_ATTRIBUTE IGdtfMacro : public IVWUnknown
 			// Sending files is possible with both, a buffer (and buffer length) or a file path
 			// If both are set, buffer is used first
 			MVRxchangeString 				PathToFile;
-			std::unique_ptr<char>			BufferToFile;
+			std::shared_ptr<char>			BufferToFile;
 			size_t							BufferToFileLength;
 			
 			bool 							RetIsOK;
