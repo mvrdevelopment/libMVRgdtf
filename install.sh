d2479f043c19ef4d2aec5c2667add5db9c781b0a
SimeonRolev <simorolev@gmail.com>
2024-10-08 13:24:17 +0300

    Install script

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 export PKG_V=$(node -p "require('./package.json').version")
-npm install --save --save-exact @mvrdevelopment/lib-gdtf-win32@$PKG_V @mvrdevelopment/lib-gdtf-linux@$PKG_V @mvrdevelopment/lib-gdtf-darwin@$PKG_V
+export OS=$(node -p process.platform)
+
+if npm show @mvrdevelopment/lib-gdtf-$OS@$PKG_V > /dev/null 2>&1; then
+    npm install --save --save-exact @mvrdevelopment/lib-gdtf-$OS@$PKG_V
+else
+    echo "@mvrdevelopment/lib-gdtf-$OS@$PKG_V is not available on npm."
+fi
