8840a1c17b1f9f464a6ca6af000417700f2dec7c
SimeonRolev <simorolev@gmail.com>
2024-10-10 15:51:06 +0300

    Some reverts

--- install.sh
+++
@@ -1,8 +0,0 @@
-export PKG_V=$(node -p "require('./package.json').version")
-export OS=$(node -p process.platform)
-
-if npm show @mvrdevelopment/lib-gdtf-$OS@$PKG_V > /dev/null 2>&1; then
-    npm install --save --save-exact @mvrdevelopment/lib-gdtf-$OS@$PKG_V
-else
-    echo "@mvrdevelopment/lib-gdtf-$OS@$PKG_V is not available on npm."
-fi
