34868dbcf2a444aecfc3603b3abfea79fc0a420e
Alesandro Dragnev <adragnev@vectorworks.net>
2026-09-08 09:20:48 +0300

    fixing build error

--- src/Include/IMediaRessourceVectorInterface.h
+++ src/Include/IMediaRessourceVectorInterface.h
@@ -2066,7 +2066,7 @@ class DYNAMIC_ATTRIBUTE IGdtfMacro : public IVWUnknown
 		using NetworkInterface = std::pair<std::string, uint32_t>;
 		// Allows the user to query all available network interfaces on the system, and set one of them for the MVR-xchange service to use
 		virtual VCOMError VCOM_CALLTYPE     QueryAllAvailableInterfaces( std::vector<NetworkInterface>& out ) = 0;
-		virtual VCOMError VCOM_CALLTYPE     SetNetworkInterface( const NetworkInterface& interface ) = 0;
+		virtual VCOMError VCOM_CALLTYPE     SetNetworkInterface( const NetworkInterface& inInterface ) = 0;
         
 		typedef IMVRxchangeMessage (*IMVRxchangeIncomingMessage)(const IMVRxchangeMessage& args, void* context);
 		typedef void (*IMVRxchangeReturningMessage)(const IMVRxchangeMessage& outgoingMsg, const IMVRxchangeMessage& returningMsg, void* context);
